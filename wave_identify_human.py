b0a2df7115b22911ff15648d7b4253d5ee4616c9
Arjun Goswami <llgoswamiarjunll@gmail.com>
2019-01-10 09:55:41 -0800

    Update wave_identify_human.py

--- wave_identify_human.py
+++ wave_identify_human.py
@@ -171,8 +171,8 @@ if camera_type == 'picamera':
 
 #use the value is_human and print "it's a human if the confidence score is above the threshold you decide"
 #################################edit starts##########################################                
-                if is_human > 60:
-                    print("it's a human, I am %s%% sure about it" % is_human)
+                
+  
 ###################################edit ends##########################################                    
 
         cv2.putText(frame,"FPS: {0:.2f}".format(frame_rate_calc),(30,50),font,1,(255,255,0),2,cv2.LINE_AA)
